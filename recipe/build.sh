ac709624e7697ca64509924846ceb58fdac37867
Kerry Su <me@sshockwave.net>
2022-11-21 14:14:13 +0800

    Revert "Try to remove patch_libtool"
    This reverts commit af6b5f5306b326ddeb658fcb492464e535cfc259.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,9 @@ set -ex
 
 ./autogen.sh
 ./configure --prefix="$PREFIX" --enable-shared --disable-static
+if [[ "$target_platform" == "win-64" ]]
+then
+  patch_libtool
+fi
 make -j${CPU_COUNT}
 make install
