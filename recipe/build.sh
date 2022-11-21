af6b5f5306b326ddeb658fcb492464e535cfc259
Kerry Su <me@sshockwave.net>
2022-11-21 08:09:07 +0800

    Try to remove patch_libtool

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,5 @@ set -ex
 
 ./autogen.sh
 ./configure --prefix="$PREFIX" --enable-shared --disable-static
-if [[ "$target_platform" == "win-64" ]]
-then
-  patch_libtool
-fi
 make -j${CPU_COUNT}
 make install
