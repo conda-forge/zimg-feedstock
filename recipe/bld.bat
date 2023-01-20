4e0b7e10fbddbd6eb5bf3ca151d7e4021a929cc1
Kerry Su <me@sshockwave.net>
2023-01-20 19:10:23 +0800

    Feat manually install the library

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,19 @@
-cd _msvc
-
-MSBuild.exe zimg.sln /t:dll /p:Configuration=Release /p:OutputDir="output\"
+MSBuild.exe _msvc\zimg.sln /t:dll /p:Configuration=Release /p:OutputDir="build\"
 if %ERRORLEVEL% neq 0 exit 1
 
-dir output\
-dir x64\Release\
+copy build\z.dll %LIBRARY_BIN%\zimg.dll
+copy build\z_imp.lib %LIBRARY_LIB%\zimg.lib
+del build\z.lib
+del build\z_imp.exp
 
-exit 1
+rem Future releases might add targets and they should be properly handled
+rmdir build
+if %ERRORLEVEL% neq 0 exit 1
+
+xcopy /E %SRC_DIR%\doc %LIBRARY_PREFIX%\share\doc\zimg
+
+copy %SRC_DIR%\src\zimg\api\zimg.h %LIBRARY_INC%\zimg.h
+copy %SRC_DIR%\src\zimg\api\zimg++.hpp %LIBRARY_INC%\zimg++.hpp
+
+mkdir %LIBRARY_LIB%\pkgconfig
+copy %SRC_DIR%\zimg.pc %LIBRARY_LIB%\pkgconfig
