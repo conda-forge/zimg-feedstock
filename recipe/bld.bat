3fea3876ec0fcebe8ad08bc46541fb7d3b0650e7
Kerry Su <me@sshockwave.net>
2023-01-20 15:32:02 +0800

    Debug: output metaproj file

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,8 @@
 cd _msvc
 
-MSBuild.exe zimg.sln /t:dll:Build /p:Configuration=Release /p:OutputPath="output\"
+set MSBuildEmitSolution=1
+
+MSBuild.exe zimg.sln /t:dll /p:Configuration=Release /p:OutputPath="output\" & type zimg.sln.metaproj
 
 type zimg.sln.metaproj
 if %ERRORLEVEL% neq 0 exit 1
