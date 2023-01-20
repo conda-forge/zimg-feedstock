a267d3a91a06f859fed3e4bae1cb8a12e0bd03b1
Kerry Su <me@sshockwave.net>
2023-01-20 15:00:45 +0800

    Correct path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,6 @@
 cd _msvc
 
-MSBuild.exe _msvc\zimg.sln /t:dll:Build /p:Configuration=Release /p:OutputPath="output\"
+MSBuild.exe zimg.sln /t:dll:Build /p:Configuration=Release /p:OutputPath="output\"
 if %ERRORLEVEL% neq 0 exit 1
 
 dir output\
