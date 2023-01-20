3e7e692b7e8f0eabb9a83f1b79aa2f5ffe7dcda7
Kerry Su <me@sshockwave.net>
2023-01-20 19:55:50 +0800

    Add `_msvc/` to path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,7 @@
 MSBuild.exe _msvc\zimg.sln /t:dll /p:Configuration=Release
 if %ERRORLEVEL% neq 0 exit 1
 
-set OUTPUT_DIR=x64\Release
-
-mkdir %LIBRARY_BIN%
-mkdir %LIBRARY_INC%
-mkdir %LIBRARY_LIB%\pkgconfig
+set OUTPUT_DIR=_msvc\x64\Release
 
 rem The outputs are renamed to be the same as autotools output
 mv %OUTPUT_DIR%\z.dll %LIBRARY_BIN%\zimg.dll
@@ -17,9 +13,8 @@ rem Future releases might add targets and they should be properly handled
 rmdir %OUTPUT_DIR%
 if %ERRORLEVEL% neq 0 exit 1
 
-xcopy /E %SRC_DIR%\doc %LIBRARY_PREFIX%\share\doc\zimg
-
 copy %SRC_DIR%\src\zimg\api\zimg.h %LIBRARY_INC%\zimg.h
 copy %SRC_DIR%\src\zimg\api\zimg++.hpp %LIBRARY_INC%\zimg++.hpp
 
+mkdir %LIBRARY_LIB%\pkgconfig
 copy %SRC_DIR%\zimg.pc %LIBRARY_LIB%\pkgconfig
