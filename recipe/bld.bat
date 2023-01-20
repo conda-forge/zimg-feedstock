e0de6ae32b95534f889f96d8fc490e1a54226907
Kerry Su <me@sshockwave.net>
2023-01-20 20:05:37 +0800

    Change all copy to move

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,8 +13,8 @@ rem Future releases might add targets and they should be properly handled
 rmdir %OUTPUT_DIR%
 if %ERRORLEVEL% neq 0 exit 1
 
-copy %SRC_DIR%\src\zimg\api\zimg.h %LIBRARY_INC%\zimg.h
-copy %SRC_DIR%\src\zimg\api\zimg++.hpp %LIBRARY_INC%\zimg++.hpp
+mv %SRC_DIR%\src\zimg\api\zimg.h %LIBRARY_INC%\zimg.h
+mv %SRC_DIR%\src\zimg\api\zimg++.hpp %LIBRARY_INC%\zimg++.hpp
 
 mkdir %LIBRARY_LIB%\pkgconfig
-copy %SRC_DIR%\zimg.pc %LIBRARY_LIB%\pkgconfig
+mv %SRC_DIR%\zimg.pc %LIBRARY_LIB%\pkgconfig
