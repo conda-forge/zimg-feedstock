ba890ef997eb62446a328ce58ad245076d951ead
Kerry Su <me@sshockwave.net>
2023-01-20 14:06:56 +0800

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,1 +1,4 @@
-MSBuild.exe _msvc\zimg.sln /t:Build /p:Configuration=Release
+MSBuild.exe _msvc\zimg.sln /t:dll:Build /p:Configuration=Release /p:OutputPath="output\"
+if %ERRORLEVEL% neq 0 exit 1
+
+dir output\
